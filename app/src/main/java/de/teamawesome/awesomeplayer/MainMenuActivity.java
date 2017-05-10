2f34000f8983708945bbe38243aa1e6d4428fa83
Trikley <dexter.d@gmx.de>
2017-05-10 17:10:14 +0200

    Initial commit

---
+++ app/src/main/java/de/teamawesome/awesomeplayer/MainMenuActivity.java
@@ -0,0 +1,17 @@
+package de.teamawesome.awesomeplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.prefs.Preferences;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        System.out.println("FUCK YOUUUUUUUUU!!!!!!");
+        System.out.println("AGAIIIIIIINNNNNN!!!!!!");
+    }
+}
