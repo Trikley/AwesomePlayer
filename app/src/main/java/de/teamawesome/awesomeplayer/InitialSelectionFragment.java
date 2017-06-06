506bbbdc124259299437e8eabfc87008f227651a
tooxoot <sv30192+git@gmail.com>
2017-06-06 18:57:35 +0200

    {#6} first interface implementation - applied to the initial fragment

--- app/src/main/java/de/teamawesome/awesomeplayer/InitialSelectionFragment.java
+++ app/src/main/java/de/teamawesome/awesomeplayer/InitialSelectionFragment.java
@@ -3,106 +3,69 @@ package de.teamawesome.awesomeplayer;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import de.teamawesome.awesomeplayer.listFragments.ListBundles;
+import de.teamawesome.awesomeplayer.listFragments.MediaListFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link InitialSelectionFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link InitialSelectionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InitialSelectionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public InitialSelectionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InitialSelectionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InitialSelectionFragment newInstance(String param1, String param2) {
-        InitialSelectionFragment fragment = new InitialSelectionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentListener fListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_selection, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_initial_selection, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        // Listener Button ALL
+        view.findViewById(R.id.Button_All).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fListener.onFragmentButtonClick(R.id.Button_All);
+            }
+        });
+
+        // Listener Button ALBUMS
+        view.findViewById(R.id.Button_Albums).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fListener.onFragmentButtonClick(R.id.Button_Albums);
+            }
+        });
+
+        // Listener Button PLAYLISTS
+        view.findViewById(R.id.Button_Playlists).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fListener.onFragmentButtonClick(R.id.Button_Playlists);
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof FragmentListener) {
+            fListener = (FragmentListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement FragmentListener");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        fListener = null;
     }
 }
--- app/src/main/java/de/teamawesome/awesomeplayer/MainMenuActivity.java
+++ app/src/main/java/de/teamawesome/awesomeplayer/MainMenuActivity.java
@@ -5,26 +5,58 @@ import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.teamawesome.awesomeplayer.listFragments.AlbumsListFragment;
 import de.teamawesome.awesomeplayer.listFragments.ListBundles;
+import de.teamawesome.awesomeplayer.listFragments.MediaListFragment;
 import de.teamawesome.awesomeplayer.listFragments.PlaylistsListFragment;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements FragmentListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         // Create new fragment and transaction
-        Fragment listFragment = new PlaylistsListFragment();
-        listFragment.setArguments(ListBundles.PLAYLIST_BUNDLE.get());
+        Fragment initialFragment = new InitialSelectionFragment();
+        initialFragment.setArguments(ListBundles.PLAYLIST_BUNDLE.get());
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
         // Replace whatever is in the fragment_container view with this fragment,
         // and add the transaction to the back stack if needed
-        transaction.replace(R.id.MainContainer, listFragment);
+        transaction.replace(R.id.MainContainer, initialFragment);
         transaction.addToBackStack(null);
 
         // Commit the transaction
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Bundle arguments, Object caller) {
+
+    }
+
+    @Override
+    public void onFragmentButtonClick(int id) {
+        switch(id){
+            case R.id.Button_All:
+                replaceFragment(new MediaListFragment(), ListBundles.MEDIA_BUNDLE.get());
+                break;
+            case R.id.Button_Albums:
+                replaceFragment(new AlbumsListFragment(), ListBundles.ALBUM_BUNDLE.get());
+                break;
+            case R.id.Button_Playlists:
+                replaceFragment(new PlaylistsListFragment(), ListBundles.PLAYLIST_BUNDLE.get());
+                break;
+            default: break;
+        }
+    }
+
+
+    protected void replaceFragment(Fragment newFragment, Bundle arguments){
+        newFragment.setArguments(arguments);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.MainContainer, newFragment);
+        transaction.addToBackStack(null);
         transaction.commit();
     }
 }
