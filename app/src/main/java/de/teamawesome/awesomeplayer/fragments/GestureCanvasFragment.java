0b41ff67b188afef16eeaa9833f998e8e529e782
tooxoot <sv30192+git@gmail.com>
2017-06-12 13:28:29 +0200

    {#9} Added reachable gesture canvas for Screen VI

---
+++ app/src/main/java/de/teamawesome/awesomeplayer/fragments/GestureCanvasFragment.java
@@ -0,0 +1,79 @@
+package de.teamawesome.awesomeplayer.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.teamawesome.awesomeplayer.R;
+import de.teamawesome.awesomeplayer.fragments.listFragments.CursorListFragment;
+
+/**
+ * This {@link android.app.Fragment} represents the Gesture interface from Screen VI.
+ */
+public class GestureCanvasFragment extends Fragment {
+    private FragmentListener fragmentListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gesture_canvas, container, false);
+    }
+
+    /**
+     * onAttach and onDetach are used to handle the assignment of 'fragmentListener'
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentListener) {
+            fragmentListener = (FragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement FragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        fragmentListener = null;
+    }
+
+    /**
+     * This class is used to handle the Gesture recognition.
+     */
+    private class CursorListOnTouchListener extends GestureDetector.SimpleOnGestureListener implements View.OnTouchListener{
+
+        // The CursorListFragment used to handle onFling events
+        private CursorListFragment attachedListFragment;
+        // The GestureDetector needed to handle the gesture recognition;
+        private GestureDetector gd = new GestureDetector(getActivity(),this);
+
+        CursorListOnTouchListener(CursorListFragment _attachCursorListFragment){
+            super();
+            attachedListFragment = _attachCursorListFragment;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gd.onTouchEvent(event);
+        }
+
+        /**
+         * Catches all double taps and triggers the fragment transition on the activity;
+         */
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            fragmentListener.onFragmentDoubleTap(this);
+            return true;
+        }
+    }
+
+}
