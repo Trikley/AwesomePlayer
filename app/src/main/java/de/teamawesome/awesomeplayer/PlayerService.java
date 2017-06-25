24d84e1db03e0343a7309a59baea8128f7f9aba9
Trikley <dexter.d@gmx.de>
2017-06-25 19:48:52 +0200

    Tristan spielt

--- app/src/main/java/de/teamawesome/awesomeplayer/PlayerService.java
+++ app/src/main/java/de/teamawesome/awesomeplayer/PlayerService.java
@@ -2,15 +2,75 @@ package de.teamawesome.awesomeplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 
-public class PlayerService extends Service {
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class PlayerService extends Service implements Runnable {
+
+    private boolean keepRunning;
+    private MediaPlayer mediaPlayer;
+    private Queue<String> audioPathQueue;
+
     public PlayerService() {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        audioPathQueue = new ConcurrentLinkedQueue<>();
+        new Thread(this, "PlayerServiceTherad").start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        keepRunning = false;
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new PlayerBind();
+    }
+
+    private void addSongToQueue(String pathToSong) {
+        audioPathQueue.add(pathToSong);
+    }
+
+    private void startMediaPlayer(String pathToSong) throws IOException{
+        if(mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(pathToSong);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while(keepRunning) {
+                if(audioPathQueue.size()<1) {
+                    continue;
+                }
+                startMediaPlayer(audioPathQueue.poll());
+            }
+        }catch (IOException e) {
+            //TODO Make useful catch-clause
+            e.printStackTrace();
+            this.stopSelf();
+        }
+    }
+
+
+
+    public class PlayerBind extends Binder {
+        public void playSongWhenReady(String pathToSong) {
+            PlayerService.this.addSongToQueue(pathToSong);
+        }
     }
 }
--- app/src/main/java/de/teamawesome/awesomeplayer/fragments/PlayerFragment.java
+++ app/src/main/java/de/teamawesome/awesomeplayer/fragments/PlayerFragment.java
@@ -37,7 +37,7 @@ public class PlayerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        return inflater.inflate(R.layout.activity_screen_ii, container, false);
     }
 
     /**
