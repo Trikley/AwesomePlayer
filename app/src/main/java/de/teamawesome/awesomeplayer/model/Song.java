8ef00c01d2f3bdd19dd381ab811f8db573d0ec79
tooxoot <sv30192+git@gmail.com>
2017-06-28 13:38:42 +0200

    {#29} Added song extracting method for cursors

--- app/src/main/java/de/teamawesome/awesomeplayer/model/Song.java
+++ app/src/main/java/de/teamawesome/awesomeplayer/model/Song.java
@@ -1,5 +1,9 @@
 package de.teamawesome.awesomeplayer.model;
 
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
 public class Song {
 
     //MEDIASTORE.AUDIO.MEDIA._ID
@@ -25,12 +29,38 @@ public class Song {
     //MEDIASTORE.AUDIO.MEDIA.ARTIST
     private String artist;
 
+    private Song(){};
+
     public Song(String id, String path, String displayName,
                 String title, String duration, String albumID,
                 String albumName, String trackName, String artistID,
                 String artist) {
         super();
     }
+
+    public static Song[] extractSongsFromCursor(Cursor cursor){
+        ArrayList<Song> returnedSongs = new ArrayList<Song>();
+        while(cursor.moveToNext()){
+            Song currentSong = new Song();
+
+            currentSong.id = cursor.getString(cursor.getColumnIndex("_ID"));
+            currentSong.path = cursor.getString(cursor.getColumnIndex("DATA"));
+            currentSong.displayName = cursor.getString(cursor.getColumnIndex("DISPLAY_NAME"));
+            currentSong.title = cursor.getString(cursor.getColumnIndex("TITLE"));
+            currentSong.duration = cursor.getString(cursor.getColumnIndex("DURATION"));
+
+            currentSong.albumID = cursor.getString(cursor.getColumnIndex("ALBUM_ID"));
+            currentSong.albumName = cursor.getString(cursor.getColumnIndex("ALBUM"));
+            currentSong.trackNumber = cursor.getString(cursor.getColumnIndex("TRACK"));
+
+            currentSong.artistID = cursor.getString(cursor.getColumnIndex("ARTIST_ID"));
+            currentSong.artist = cursor.getString(cursor.getColumnIndex("ARTIST"));
+
+            returnedSongs.add(currentSong);
+        }
+        return (Song[]) returnedSongs.toArray();
+    }
+
 
     public String getId() {
         return id;
